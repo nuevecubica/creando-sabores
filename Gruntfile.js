b5c2e923482d4723f47c4fc99a3adf729302db1b
Ignacio Lago <ignacio@glue.gl>
2014-05-28 15:55:05 +0200

    openshift package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,9 +113,13 @@ module.exports = function(grunt) {
 		]);
 	});
 
-	grunt.registerTask('server', function () {
-		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-		grunt.task.run(['serve:' + target]);
+	grunt.registerTask('prod', function () {});
+
+	grunt.registerTask('dev', function () {
+		grunt.task.run(['jshint']);
 	});
 
+	grunt.registerTask('default', function () {
+		grunt.task.run(['dev']);
+	});
 };
