7f46d4f68474a7420adfc7af643862815814d0d8
Ignacio Lago <ignacio@glue.gl>
2014-05-29 18:23:38 +0200

    Grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,4 +145,8 @@ module.exports = function(grunt) {
 	grunt.registerTask('default', function () {
 		grunt.task.run([grunt.config('env')]);
 	});
+
+	grunt.registerTask('build', function () {
+		grunt.task.run([grunt.config('env')]);
+	});
 };
