805978b20434d8585393869b234511b180b88f62
Ignacio Lago <ignacio@glue.gl>
2014-05-30 11:32:06 +0200

    Users schema

--- models/users.js
+++ models/users.js
@@ -14,7 +14,10 @@ User.add({
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: false }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone' }
+	isAdmin: { type: Boolean, label: 'Superadmin', note: 'Can access Keystone.' },
+	isConfirmed: { type: Boolean, label: 'Confirmed', note: 'Has confirmed email address. Can publish.' },
+	isChef: { type: Boolean, label: 'Chef', note: 'An official chef. Admin role.' },
+	isBanned: { type: Boolean, label: 'Banned', note: 'Cannot login.' }
 });
 
 // Provide access to Keystone
@@ -22,10 +25,24 @@ User.schema.virtual('canAccessKeystone').get(function() {
 	return this.isAdmin;
 });
 
+// Rights to publish
+User.schema.virtual('canPublish').get(function() {
+	return this.isAdmin || this.isConfirmed || this.isChef;
+});
+
+// Rights to admin
+User.schema.virtual('canAdmin').get(function() {
+	return this.isAdmin || this.isChef;
+});
+
+// Rights to login
+User.schema.virtual('canLogin').get(function() {
+	return !this.isBanned;
+});
 
 /**
  * Registration
  */
 
-User.defaultColumns = 'name, email, isAdmin';
+User.defaultColumns = 'name, email, isAdmin, isChef, canPublish';
 User.register();
