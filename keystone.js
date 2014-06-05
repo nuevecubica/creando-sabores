c8bd86fd615bd9ca9e30c5c18b7e2f2e7b2a5b49
Ignacio Lago <ignacio@glue.gl>
2014-06-05 17:42:57 +0200

    Disabled MongoDB debugging

--- keystone.js
+++ keystone.js
@@ -81,14 +81,12 @@ MongoDB Environment:
 	MONGO_URL
 */
 
-	console.log("MongoDB Connection:\n\
-		DB: " 	+ process.env.MONGODB_DATABASE + "\n\
-		HOST: " + process.env.MONGODB_HOST + "\n\
-		PORT: " + process.env.MONGODB_PORT + "\n\
-		USER: " + process.env.MONGODB_USERNAME + "\n\
-		PASS: " + process.env.MONGODB_PASSWORD + "\n\
-		URL:  " + process.env.MONGO_URL);
-
+// console.log("MongoDB Connection:\n\
+// 	DB: " 	+ process.env.MONGODB_DATABASE + "\n\
+// 	HOST: " + process.env.MONGODB_HOST + "\n\
+// 	PORT: " + process.env.MONGODB_PORT + "\n\
+// 	USER: " + process.env.MONGODB_USERNAME + "\n\
+// 	URL:  " + process.env.MONGO_URL);
 
 if (process.env.MONGO_URL) {
     keystone.set('mongo url', process.env.MONGO_URL);
