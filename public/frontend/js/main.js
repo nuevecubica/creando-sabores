5bcaaee53f043015ae288119000dd40784a0ce6a
Ignacio Lago <ignacio@glue.gl>
2014-06-09 18:13:44 +0200

    Cleaning css code

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,9 +1,13 @@
+$(window).load(function() {
+  $('body').removeClass('preload');
+});
+
 $(document).ready(function() {
 	$('.demo.menu .item').tab();
 	$('#menu').on('click', function(){
-		$('#menu-box').toggleClass('open');
+		$('#menu-box').addClass('open').removeClass('close');
 	});
 	$('#menu-close').on('click', function(){
-		$('#menu-box').removeClass('open');
+		$('#menu-box').removeClass('open').addClass('close');
 	});
 });
