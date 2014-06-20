e9f71cbb569029b64517cd67d99a1f512ce69dfa
Ignacio Lago <ignacio@glue.gl>
2014-06-20 14:12:35 +0200

    Multi-language for tests

--- app-test-init.js
+++ app-test-init.js
@@ -15,7 +15,8 @@ if (!process.env.NODE_ENV) {
 }
 
 var config = require('./config-test.js'),
-  keystone = require('keystone');
+  keystone = require('keystone'),
+  i18n = require('i18n');
 
 keystone.init(config.keystone.init);
 
@@ -33,6 +34,13 @@ keystone.set('routes', require('./routes'));
 keystone.set('email locals', config.keystone['email locals']);
 keystone.set('email rules', config.keystone['email rules']);
 keystone.set('email tests', require('./routes/emails'));
+
+// Configure i18n
+i18n.configure({
+  locales: ['es'],
+  defaultLocale: 'es',
+  directory: __dirname + '/locales'
+});
 
 keystone.set('nav', {
   'users': 'users',
