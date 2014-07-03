94b76ec050d9c0b343ce6f9334881a67d3308c48
Miguel Sánchez <miguel@glue.gl>
2014-07-03 18:23:43 +0200

    Profile: Changed editables style

--- public/frontend/js/profile.js
+++ public/frontend/js/profile.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function() {
-	$('.set-editable').on('focus').addClass('focus');
+	$('.set-editable').on('focus').toggleClass('focus');
 
 	$('#edit.button-manage').on('click', function() {
 		$('body').toggleClass('mode-editable');
+		$('.set-editable').attr('contenteditable', true);
+		console.log('------')
 	})
 });
