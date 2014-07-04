cc9ea7b8eeb27f9348189d29bcf9c7a91d1559cf
Miguel Sánchez <miguel@glue.gl>
2014-07-04 14:43:08 +0200

    Buttons: Fixed 5px margin for edition buttons Header: Added container for edition buttons Menu: Added functionality for edition buttons

--- public/frontend/js/profile.js
+++ public/frontend/js/profile.js
@@ -1,10 +1,40 @@
 
 $(document).ready(function() {
-	$('.set-editable').on('focus').toggleClass('focus');
+	$('.set-editable').on('focus', function() {
+		console.log('FOCUS');
+		$(this).toggleClass('focus');
+	}).on('focusout', function() {
+		$(this).toggleClass('focus');
+	}).on('keydown', function(e) {
+
+		// If not return or arrow keys
+		if(e.which != 8 && e.which != 37 && e.which != 38 && e.which != 39 && e.which != 40) {
+			// Limit name text size
+			if($(this).data('length') && $(this).text().length > $(this).data('length')) {
+				e.preventDefault();
+			}
+		}
+	}).on('paste', function(e) {
+		e.preventDefault();
+	}).on('drop', function(e) {
+		e.preventDefault();
+	});
+
+	$('.set-editable.one-line').on('keypress', function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+		}
+	}).on('paste', function(e) {
+		e.preventDefault();
+	});
 
 	$('#edit.button-manage').on('click', function() {
-		$('body').toggleClass('mode-editable');
+		$('body').addClass('mode-editable');
 		$('.set-editable').attr('contenteditable', true);
-		console.log('------')
+	})
+
+	$('#cancel.button-manage').on('click', function() {
+		$('body').removeClass('mode-editable');
+		$('.set-editable').attr('contenteditable', false);
 	})
 });
