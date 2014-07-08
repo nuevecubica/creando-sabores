bfb3d4f45d92ef70766ffcec4a389e7559a03986
Miguel Sánchez <miguel@glue.gl>
2014-07-08 16:29:28 +0200

    Profile: Added keys control in user profile fields

--- public/frontend/js/profile.js
+++ public/frontend/js/profile.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   }).on('keydown', function(e) {
 
     // If not return or arrow keys
-    if (e.which != 8 && e.which != 37 && e.which != 38 && e.which != 39 && e.which != 40) {
+    if (e.which !== 8 && e.which !== 37 && e.which !== 38 && e.which !== 39 && e.which !== 40) {
       // Limit name text size
       if ($(this).data('length') && $(this).text().length > $(this).data('length')) {
         e.preventDefault();
@@ -20,7 +20,7 @@ $(document).ready(function() {
   });
 
   $('.set-editable.one-line').on('keypress', function(e) {
-    if (e.which == 13) {
+    if (e.which !== 13) {
       e.preventDefault();
     }
   }).on('paste', function(e) {
@@ -30,10 +30,10 @@ $(document).ready(function() {
   $('#edit.button-manage').on('click', function() {
     $('body').addClass('mode-editable');
     $('.set-editable').attr('contenteditable', true);
-  })
+  });
 
   $('#cancel.button-manage').on('click', function() {
     $('body').removeClass('mode-editable');
     $('.set-editable').attr('contenteditable', false);
-  })
+  });
 });
