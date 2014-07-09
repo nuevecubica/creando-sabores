d75fed841454a9794e9b6577d8d8ce9b727a27e4
Miguel Sánchez <miguel@glue.gl>
2014-07-09 08:17:33 +0200

    Fixed bug in config updates.

--- updates/0.0.3-configs.js
+++ updates/0.0.3-configs.js
@@ -7,7 +7,7 @@ var configs = [{
   value: '1, 2, 3, 4, 5, 6, 7, 8, 9, 10'
 }, {
   name: 'grid_size_desktop_home',
-  value: 'small, small, small, medium, large, small, small, small, medium, large'
+  value: 'small, small, small, medium, large, small, small, small, medium, small'
 }, {
   name: 'grid_order_tablet_home',
   value: '1, 2, 3, 5, 6, 7, 8, 4, 9, 10'
@@ -25,7 +25,7 @@ var configs = [{
   value: '1, 2, 3, 4, 5, 6, 7, 8, 9, 10'
 }, {
   name: 'grid_size_desktop_recipes',
-  value: 'small, small, small, medium, large, small, small, small, medium, large'
+  value: 'small, small, small, medium, large, small, small, small, medium, small'
 }, {
   name: 'grid_order_tablet_recipes',
   value: '1, 2, 3, 5, 6, 7, 8, 4, 9, 10'
