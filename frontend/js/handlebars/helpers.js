8f40ac8681ae740fdd258cb391ca4e4b0d6aed73
Ignacio Lago <ignacio@glue.gl>
2014-07-14 12:22:55 +0200

    HBS helper: fixed and clean

--- frontend/js/handlebars/helpers.js
+++ frontend/js/handlebars/helpers.js
@@ -1,28 +1,29 @@
 /* global Handlebars */
 
 Handlebars.registerHelper('formatTime', function(time) {
-	return (time < 60) ? time + ' min' : (time / 60).toFixed(2) + ' h';
+  return (time < 60) ? time + ' min' : (time / 60).toFixed(2) + ' h';
 });
 
 Handlebars.registerHelper('formatOfficial', function(isOfficial) {
-	if(isOfficial) {
-		return new Handlebars.SafeString('<div class="official"><img src="/images/ribbon.png"></div>');
-	}
+  if (isOfficial) {
+    return new Handlebars.SafeString('<div class="official"><img src="/images/ribbon.png"></div>');
+  }
 });
 
 Handlebars.registerHelper('formatIcons', function(number, icon) {
-	var ret = '';
+  var ret = '';
 
-	var number = Math.floor(number) || 0;
+  number = Math.floor(number) || 0;
 
-	for(var i = 0; i < 5; i++) {
-		if (number === 0) {
-			ret = ret + '<i class="icon-chef-' + icon + ' icon off"></i>';
-		} else {
-			ret = ret + '<i class="icon-chef-' + icon + ' icon on"></i>';
-			number = number - 1;
-		}
-	}
+  for (var i = 0; i < 5; i++) {
+    if (number === 0) {
+      ret = ret + '<i class="icon-chef-' + icon + ' icon off"></i>';
+    }
+    else {
+      ret = ret + '<i class="icon-chef-' + icon + ' icon on"></i>';
+      number = number - 1;
+    }
+  }
 
-	return new Handlebars.SafeString(ret);
+  return new Handlebars.SafeString(ret);
 });
