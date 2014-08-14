d4c6cee6b9387e44f08ab66e8cf8ee4fadc39bea
Miguel Sánchez <miguel@glue.gl>
2014-08-14 13:31:26 +0200

    Fixed recipe and no recipe (new recipe) values between recipe.js and front

--- routes/views/recipe.js
+++ routes/views/recipe.js
@@ -7,41 +7,56 @@ exports = module.exports = function(req, res) {
   var locals = res.locals,
     view = new keystone.View(req, res);
 
+  locals.data = {};
+
   // Init locals
-  locals.section = 'recipe';
+  if (req.params.recipe) {
+    locals.section = 'recipe';
+    locals.isNew = false;
+  }
+  else {
+    locals.section = 'new-recipe';
+    locals.isNew = true;
+    locals.own = true;
+  }
+
   locals.editable = true;
   locals.manageable = true;
 
   locals.filters = {
     recipe: req.params.recipe
   };
-  locals.data = {};
+
 
   // load recipe
   view.on('init', function(next) {
 
-    var q = Recipe.model.findOne({
-      state: 1,
-      slug: locals.filters.recipe
-    }).populate('author');
+    if (!locals.isNew) {
+      var q = Recipe.model.findOne({
+        state: 1,
+        slug: locals.filters.recipe
+      }).populate('author');
 
-    q.exec(function(err, result) {
-      if (!err) {
-        locals.data.recipe = result;
-        if (result) {
-          locals.title = result.title + ' - ' + res.__('Recipe');
+      q.exec(function(err, result) {
+        if (!err) {
+          locals.data.recipe = result;
+          if (result) {
+            locals.title = result.title + ' - ' + res.__('Recipe');
 
-          if (req.user) {
-            locals.own = (req.user._id.toString() === result.author._id.toString());
+            if (req.user) {
+              locals.own = (req.user._id.toString() === result.author._id.toString());
+            }
+            else {
+              locals.own = false;
+            }
           }
-          else {
-            locals.own = false;
-          }
-
         }
-      }
-      next(err);
-    });
+        next(err);
+      });
+    }
+    else {
+      next(null);
+    }
   });
 
   // Render the view
