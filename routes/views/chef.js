6fd1576552fe337b6eda3dbb2074770276bf53cd
Anxo Soto <anxo@glue.gl>
2014-09-04 13:09:29 +0200

    Profile-public: Cambios de estilo.

--- routes/views/chef.js
+++ routes/views/chef.js
@@ -1,7 +1,8 @@
 var keystone = require('keystone'),
   User = keystone.list('User'),
   Recipe = keystone.list('Recipe'),
-  formResponse = require('../../utils/formResponse.js');
+  formResponse = require('../../utils/formResponse.js'),
+  async = require('async');
 
 exports = module.exports = function(req, res) {
 
@@ -10,8 +11,6 @@ exports = module.exports = function(req, res) {
 
   var getPublicRecipes = function(userId, cb) {
     var back = '..';
-
-    // Get
     var q = Recipe
       .paginate({
         page: req.query.page || 1,
@@ -30,37 +29,40 @@ exports = module.exports = function(req, res) {
       });
   };
 
-  var getUser = function(username, cb) {
-    var q = User.model.findOne({
-      username: req.params.username
-    });
-    q.exec(function(err, result) {
-      if (!err && result) {
-        locals.profile = result;
-      }
-      else {
-        return res.notfound(res.__('Not found'));
-      }
-      cb(result);
-    });
-  };
-
-
   locals.section = 'publicProfile';
 
-  getUser(req.params.username, function(profile) {
+  async.waterfall([
 
-    switch (req.params.section) {
-      case 'recetas':
-        /* falls through */
-      default:
-        getPublicRecipes(profile._id, function(recipes) {
-          locals.subsection = 'recipes';
-          locals.recipes = recipes || [];
-          view.render('chef');
-        });
+    function(next) {
+      var q = User.model.findOne({
+        username: req.params.username
+      });
+      q.exec(function(err, result) {
+        if (!err && result) {
+          locals.profile = result;
+        }
+        else {
+          return res.notfound(res.__('Not found'));
+        }
+        next(err, result);
+      });
+    },
+
+    function(profile, next) {
+      switch (req.params.section) {
+        case 'recetas':
+          /* falls through */
+        default:
+          getPublicRecipes(profile._id, function(recipes) {
+            locals.subsection = 'recipes';
+            locals.recipes = recipes || [];
+            view.render('chef');
+          });
+      }
+
     }
 
-  });
+  ]);
+
 
 };
