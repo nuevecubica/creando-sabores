74aa191b9ddc982224c50238bb922532a0c2457f
Anxo Soto <anxo@glue.gl>
2014-09-17 18:33:46 +0200

    Recipe/Like: JS cliente like/unlike. Beta.

--- frontend/js/participants.js
+++ frontend/js/participants.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
     'order': order
   });
 
+  $('.like-button').click(function() {
+    var $this = $(this);
+    var slug = $this.closest('.column').find('a').first().prop('href').split('/')[4];
+    var action = $this.data('action') ? $this.data('action') : 'like';
+    if (action !== 'like' && action !== 'unlike') {
+      return;
+    }
+    var url = '/api/v1/recipe/' + slug + '/' + action;
+    $this.data('action', 'none');
+    console.log(action);
+    var jQXhr = $.ajax({
+      url: url,
+      type: 'PUT',
+      contentType: 'application/json',
+      success: function(data) {
+        var $counter = $this.siblings('.like-counter');
+        $counter.html(data.likes);
+        $this.data('action', action === 'like' ? 'unlike' : 'like');
+      }
+    });
+  });
+
 });
