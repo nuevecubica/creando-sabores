fc29e6d19b3c84dece4190f63f8875389e4d2eb4
Ignacio Lago <ignacio@glue.gl>
2014-09-22 14:16:35 +0200

    Changes to be committed: 	modified:   services/index.js

--- services/index.js
+++ services/index.js
@@ -9,10 +9,7 @@
  */
 
 exports.recipe = require('./recipe');
-<<<<<<< HEAD
 exports.user = require('./user');
-=======
->>>>>>> 0eb30196565f3b2079f0b89581c8eaab4d2d5c3b
 exports.recipeList = require('./recipeList');
 exports.pageHeader = require('./pageHeader');
 exports.config = require('./config');
