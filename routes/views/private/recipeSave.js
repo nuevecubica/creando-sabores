fa2503f39c57367b6d2acdb9dec0da707749159c
Ignacio Lago <ignacio@glue.gl>
2014-09-26 11:55:51 +0200

    Service: recipe depends on recipeList. Controller: recipeSave uses services.

--- routes/views/private/recipeSave.js
+++ routes/views/private/recipeSave.js
@@ -3,7 +3,8 @@ var async = require('async'),
   Recipe = keystone.list('Recipe'),
   Contest = keystone.list('Contest'),
   clean = require('../../../utils/cleanText.js'),
-  formResponse = require('../../../utils/formResponse.js');
+  formResponse = require('../../../utils/formResponse.js'),
+  service = require('../../../services');
 
 var recipeData = function(req, orig) {
   // Clean data
@@ -83,21 +84,23 @@ var recipeEdit = function(req, res) {
     recipeSlug = req.params.recipe,
     back = '..';
 
-  var query = {
-    slug: recipeSlug,
+  var options = {
+    slug: recipeSlug
   };
 
   if (!req.user.isAdmin) {
-    query.author = userId;
+    options.author = userId;
   }
 
   // Get
-  var q = Recipe.model.findOne(query).exec(function(err, recipe) {
+  service.recipe.recipe.get(options, function(err, result) {
     if (err) {
       console.error('recipeEdit:', err);
       return formResponse(req, res, back, 'Error: Unknown error', false);
     }
-    else if (recipe) {
+    else if (result) {
+
+      var recipe = result.recipe;
 
       // Data
       var data = recipeData(req, recipe);
@@ -112,7 +115,7 @@ var recipeEdit = function(req, res) {
       }
 
       // Save
-      recipe.getUpdateHandler(req).process(data, {
+      recipe._document.getUpdateHandler(req).process(data, {
         fields: 'title,description,ingredients,procedure,portions,time,difficulty,header'
       }, function(err) {
         if (err) {
@@ -141,6 +144,7 @@ var recipeNew = function(req, res) {
     if (data === null) {
       return formResponse(req, res, back, 'Missing data', false);
     }
+
 
     var addRecipe = function() {
       recipe.getUpdateHandler(req).process(data, {
@@ -175,7 +179,6 @@ var recipeNew = function(req, res) {
     }
   }
   else {
-    console.log(req);
     return formResponse(req, res, back, false, false);
   }
 };
