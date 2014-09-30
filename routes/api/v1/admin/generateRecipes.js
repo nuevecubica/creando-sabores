c6619fecda6cbcc3841e01c8575a066369bb4164
Ignacio Lago <ignacio@glue.gl>
2014-09-30 13:52:16 +0200

    Fixed: recipe generator

--- routes/api/v1/admin/generateRecipes.js
+++ routes/api/v1/admin/generateRecipes.js
@@ -110,16 +110,24 @@ function generateRecipes(from, to) {
   var sectionGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var sectionVideoGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  var maxVideorecipes = 20,
+    countVideorecipes = 0;
+
   /*
     New final recipe object
   */
   function generateNewRecipe() {
-    var isVideorecipe = (faker.random.number(1000) >= 800);
+    var isVideorecipe = (countVideorecipes < maxVideorecipes) ? (faker.random.number(1000) >= 800) : false;
+    if (isVideorecipe) {
+      ++countVideorecipes;
+    }
+
     var homeHeader = (faker.random.number(1000) >= 800);
     var homeGridPos = faker.random.array_element_pop(homeGrid);
     var sectionHeader = (faker.random.number(1000) >= 800);
     var sectionGridPos = faker.random.array_element_pop(isVideorecipe ? sectionVideoGrid : sectionGrid);
     var isPromoted = (homeHeader || sectionHeader || homeGridPos > 0 || sectionGridPos > 0);
+
 
     return {
       'description': '<p>' + firstUpperCase(faker.Recipe.findRecipe()) + '. ' + firstUpperCase(faker.Lorem.paragraph()) + '.</p>',
