2d6ee70927b1dc7109b9cb0b2993f67990b939e8
Ignacio Lago <ignacio@glue.gl>
2014-10-01 14:26:01 +0200

    Docker: now loaded via Vagrant

--- docker/install_osx.fish
+++ docker/install_osx.fish
@@ -1,12 +1,5 @@
 #!/usr/local/bin/fish
 
-# Check VirtualBox
-set VBX_DIR "/Applications/VirtualBox.app/"
-if test ! -d "$VBX_DIR"
-  echo "VirtualBox required. Install VirtualBox: http://virtualbox.org/"
-  exit 1
-end
-
 # Check Homebrew
 if test -z (which brew)
   echo "Homebrew not found! Installing..."
@@ -15,39 +8,13 @@ if test -z (which brew)
   exit 1
 end
 
+# Add Cask
+brew tap caskroom/homebrew-cask
 # Install
 brew update
-brew install docker
-brew install boot2docker
+brew install brew-cask
 
-if test -d $HOME/.boot2docker
-  echo "Warning: boot2docker config directory found. Skipping init."
-else
-  boot2docker init
-  curl "http://static.dockerfiles.io/boot2docker-v1.2.0-virtualbox-guest-additions-v4.3.14.iso" > "$HOME/.boot2docker/boot2docker.iso"
-end
+brew cask install virtualbox
+brew cask install vagrant
 
-boot2docker stop
-VBoxManage sharedfolder add "boot2docker-vm" -name home -hostpath /Users
-
-# Setup env
-set BD_URL (boot2docker up 2> /dev/null | grep -o 'tcp.*')
-if test -z "$BD_URL"
-  if test -z "$DOCKER_HOST"
-    echo "Warning: boot2docker URL not found, you should setup DOCKER_HOST env variable by yourself."
-  else
-    set -U DOCKER_HOST "$DOCKER_HOST"
-  end
-else
-  set -U DOCKER_HOST "$BD_URL"
-end
-
-# Hosts entry
-set DOCKER_IP (boot2docker ip 2> /dev/null)
-if test -n (cat /etc/hosts | grep 'dockerhost')
-  echo "Warning: A hosts entry already exists. Check it manually to match $DOCKER_IP"
-else
-  echo $DOCKER_IP dockerhost | sudo tee -a /etc/hosts
-end
-
-echo "Done! Try: docker info"
+echo "Done! Run: vagrant up --provider=docker"
