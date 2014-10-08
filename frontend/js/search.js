2394fac899630049ea5471aeb3cb5e31ca020d16
Anxo Soto <anxo@glue.gl>
2014-10-08 09:54:05 +0200

    Search: Popstate works like a charm.

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -8,16 +8,13 @@ $(window).load(function() {
     });
   }
 
-  function setSearchType(name, noreload) {
+  function setSearchType(name) {
     $('#tab-all .button').removeClass('active');
     $('#tab-recipes .button').removeClass('active');
     $('#tab-videorecipes .button').removeClass('active');
     $('#tab-tips .button').removeClass('active');
     $('#tab-menus .button').removeClass('active');
     $('#tab-' + name + ' .button').addClass('active');
-    if (!noreload) {
-      $('#search-button').click();
-    }
   }
 
   function getSearchType() {
@@ -28,20 +25,53 @@ $(window).load(function() {
     return type;
   }
 
-  $('#search-button').on('click', function(e) {
-    var q = $('#search-query').val(),
-      idx = getSearchType(),
-      $results = $('#results'),
+  function getUrlState() {
+    var query = {
+      q: '',
+      idx: 'all'
+    };
+    location.search.substr(1).split("&").forEach(function(item) {
+      query[item.split("=")[0]] = item.split("=")[1];
+    });
+    return query;
+  }
+
+  function loadState(state) {
+    if (!state) {
+      state = getCurrentState();
+    }
+    $('#search-query').val(state.q);
+    setSearchType(state.idx);
+    doSearch(state);
+  }
+
+  function saveState(state) {
+    if (history.pushState) {
+      if (!state) {
+        state = getCurrentState();
+      }
+      history.pushState(state, '', '?' + $.param(state));
+    }
+  }
+
+  function getCurrentState() {
+    return {
+      q: $('#search-query').val(),
+      idx: getSearchType()
+    };
+  }
+
+  function doSearch(query) {
+    var $results = $('#results'),
       args = {
-        q: q,
-        idx: idx,
+        q: query.q,
+        idx: query.idx,
         page: 1,
         perPage: 5
       },
       url = '/api/v1/search?' + $.param(args);
 
-    e.preventDefault();
-    if (!q.length) {
+    if (!query.q.length) {
       $results.removeClass('loading loaded no-results');
       $('#results .list').html('');
       return;
@@ -70,13 +100,32 @@ $(window).load(function() {
         makePaginable('/api/v1/search', 'results', 'search', '#results .list', extraargs);
       });
     });
+  }
 
+  $('#search-button').on('click', function(e) {
+    e.preventDefault();
+    saveState();
+    loadState();
   });
 
   $('.tab a').on('click', function(e) {
-    setSearchType(this.id.substring(4));
     e.preventDefault();
+    setSearchType(this.id.substring(4));
+    saveState();
+    loadState();
   });
-  setSearchType('all', true);
+
+  window.onpopstate = function(e) {
+    console.log('Pop State:', e.state);
+    if (!e.state) {
+      loadState(window.chef.basestate);
+    }
+    else {
+      loadState(e.state);
+    }
+  };
+
+  window.chef.basestate = getUrlState();
+  loadState(window.chef.basestate);
 
 });
