cffd1323c94e9199a325c0feb6cec73ff12857c6
Alex B <alex@glue.gl>
2014-10-15 19:04:01 +0200

    finished contextual-menu

--- middlewares/initLocals.js
+++ middlewares/initLocals.js
@@ -41,28 +41,6 @@ exports.initLocals = function(req, res, next) {
     href: '/'
   }];
 
-  locals.navLinksPrivate = [{
-    label: res.__('My profile'),
-    key: 'perfil',
-    href: '/perfil'
-  }, {
-    label: res.__('Shopping list'),
-    key: 'lista-del-super',
-    href: '/'
-  }, {
-    label: res.__('My recipes'),
-    key: 'recetas',
-    href: '/perfil/recetas'
-  }, {
-    label: res.__('My menus'),
-    key: 'mis-menus',
-    href: '/'
-  }, {
-    label: res.__('My tips'),
-    key: 'mis-tips',
-    href: '/'
-  }];
-
   locals.user = req.user;
   locals.version = pkg.version + (pkg.versionName ? ('-' + pkg.versionName) : '');
   locals.ksversion = keystone.version;
