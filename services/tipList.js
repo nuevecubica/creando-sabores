c9f2b84ae3e1d3a1a8cd655c7ec0db93c13bb542
Ignacio Lago <ignacio@glue.gl>
2014-10-16 16:35:40 +0200

    Garbage day!

--- services/tipList.js
+++ services/tipList.js
@@ -2,7 +2,8 @@ var _ = require('underscore'),
   keystone = require('keystone'),
   async = require('async'),
   Tip = keystone.list('Tip'),
-  service = require('./index');
+  service = require('./index'),
+  queryMaker = require('./utils/listQueryMaker');
 
 var getAllTips = function(options, callback) {
   var own = false,
@@ -22,70 +23,11 @@ var getAllTips = function(options, callback) {
     states: ['published']
   });
 
-  var query = {};
-
-  if (options.limit) {
-    options.perPage = options.limit;
-  }
-
-  if (options.limit === 1) {
-    query = Tip.model.findOne();
-  }
-  else if (!options.page) {
-    query = Tip.model.find();
-    if (options.limit || options.perPage) {
-      query.limit(options.limit || options.perPage);
-    }
-  }
-  else {
-    query = Tip.paginate({
-      page: options.page,
-      perPage: options.perPage
-    });
-  }
-
-  if (options.slug) {
-    query.where('slug', options.slug);
-  }
-
-  if (options.authorId) {
-    query.where('author', options.authorId);
-  }
-
-  var states = options.states || [];
-
   if (options.all) {
-    states.push('draft');
+    options.states.push('draft');
   }
 
-  if (states.length) {
-    states = _.unique(states);
-    query.in('state', states);
-  }
-
-  if (options.flags && options.flags.length > 0) {
-    _.each(options.flags, function(flag) {
-      if (flag[0] === '-') {
-        query.where(flag.substr(1), false);
-      }
-      else if (flag[0] === '+') {
-        query.where(flag.substr(1), true);
-      }
-      else {
-        query.where(flag, true);
-      }
-    });
-  }
-
-  if (options.sort) {
-    query.sort(options.sort);
-  }
-
-  if (options.populate && options.populate.length) {
-    options.populate.forEach(function(pop) {
-      query.populate(pop);
-    });
-  }
+  var query = queryMaker(Tip, options);
 
   query.exec(callback || function() { /* dummy */ });
 };
