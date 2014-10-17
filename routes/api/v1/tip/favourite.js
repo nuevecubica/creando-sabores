b3eb4a3d46488a014ef6f6e76fa2ef39c65777d5
Miguel Sánchez <miguel@glue.gl>
2014-10-17 14:00:20 +0200

    favourite.js: Fixed .save()

--- routes/api/v1/tip/favourite.js
+++ routes/api/v1/tip/favourite.js
@@ -25,15 +25,21 @@ exports = module.exports = function(req, res) {
         if (err || !tip) {
           res.status(404);
           answer.error = true;
+          next(err);
         }
         else {
+          answer.success = true;
           var pos = req.user.favourites.tips.indexOf(tip._id);
+
           if (req.params.action === 'add') {
             if (pos === -1) {
               req.user.favourites.tips.push(tip._id);
               req.user.save(function(err) {
                 next(err);
               });
+            }
+            else {
+              next();
             }
           }
           else if (req.params.action === 'remove') {
@@ -43,12 +49,19 @@ exports = module.exports = function(req, res) {
                 next(err);
               });
             }
+            else {
+              next();
+            }
           }
-          answer.success = true;
         }
       });
     }
   ], function(err) {
+    if (err) {
+      answer.success = false;
+      answer.error = true;
+    }
+
     return res.apiResponse(answer);
   });
 };
