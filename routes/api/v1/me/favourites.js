4d239c3434dcc4b566bde15f1505bd271cc47183
Miguel Sánchez <miguel@glue.gl>
2014-10-17 13:58:06 +0200

    favourites.js: Fixed .save()

--- routes/api/v1/me/favourites.js
+++ routes/api/v1/me/favourites.js
@@ -24,27 +24,42 @@ exports = module.exports = function(req, res) {
         if (err || !recipe) {
           res.status(404);
           answer.error = true;
+          next(err);
         }
         else {
+          answer.success = true;
           var pos = req.user.favourites.recipes.indexOf(recipe._id);
           if (req.params.action === 'add') {
             if (pos === -1) {
               req.user.favourites.recipes.push(recipe._id);
-              req.user.save();
+              req.user.save(function(err) {
+                next(err);
+              });
+            }
+            else {
+              next();
             }
           }
           else if (req.params.action === 'remove') {
             if (pos !== -1) {
               req.user.favourites.recipes.splice(pos, 1);
-              req.user.save();
+              req.user.save(function() {
+                next(err);
+              });
+            }
+            else {
+              next();
             }
           }
-          answer.success = true;
         }
-        return next(err);
       });
     }
   ], function(err) {
+    if (err) {
+      answer.success = false;
+      answer.error = true;
+    }
+
     return res.apiResponse(answer);
   });
 
