29a668849238e6e7b39b28b50acc986e807feb32
Miguel Sánchez <miguel@glue.gl>
2014-10-20 11:35:09 +0200

    index.js: Added route for tips generator

--- routes/index.js
+++ routes/index.js
@@ -145,6 +145,7 @@ exports = module.exports = function(app) {
   app.get('/api/v1/tips/popular', routes.api.v1.tip.tips.popular);
   //-- Admin
   app.get('/api/v1/admin/generate/recipes', middleware.requireAdminApi, routes.api.v1.admin.generate.generateRecipes);
+  app.get('/api/v1/admin/generate/tips', middleware.requireAdminApi, routes.api.v1.admin.generate.generateTips);
   app.get('/api/v1/admin/generate/test', middleware.requireAdminApi, routes.api.v1.admin.generate.generateTest.middleware);
   //---- Elasticsearch
   app.get('/api/v1/admin/es/ping', routes.api.v1.admin.goldfinder.ping);
