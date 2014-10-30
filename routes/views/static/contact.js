fe8b37109c5abfd025d64b8bfc73febb3e5069c8
Ignacio Lago <ignacio@glue.gl>
2014-10-30 18:31:39 +0100

    Contact controller

--- routes/views/static/contact.js
+++ routes/views/static/contact.js
@@ -1,4 +1,6 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+  service = require(__base + 'services'),
+  clean = require(__base + 'utils/cleanText.js');
 
 exports = module.exports = function(req, res) {
 
@@ -10,7 +12,33 @@ exports = module.exports = function(req, res) {
   locals.hideSocial = true;
   locals.title = res.__('Contact');
 
+  view.on('post', {
+    action: 'contact-form'
+  }, function(next) {
+
+    if (req.body.url) {
+      return;
+    }
+
+    req.body.name = req.user ? req.user.name : clean(req.body.name, ['plaintext', 'oneline', ['maxlength', 50]]);
+    req.body.email = req.email ? req.user.email : clean(req.body.email, ['plaintext', 'oneline', ['maxlength', 50]]);
+    req.body.comment = clean(req.body.comment, ['plaintext', ['maxlength', 300]]);
+
+    service.email.send('contact-form', {
+      to: {
+        name: 'Contacto',
+        email: keystone.get('site').email
+      },
+      globalMergeVars: {
+        subject: 'Contacto formulario',
+        name: req.body.name,
+        email: req.body.email,
+        topic: req.body.comment
+      }
+    }, function(){});
+
+  });
+
   // Render the view
   view.render('static/contact');
-
 };
