421618c1bc86f4ea60b559bb17963476efd5518e
Ignacio Lago <ignacio@glue.gl>
2014-10-30 13:18:10 +0100

    Production configuration (except FB and G+ logins)

---
+++ configs/config-production.js
@@ -0,0 +1,33 @@
+var _ = require('underscore'),
+  defaults = require(__base + 'configs/config-default');
+
+_.deepDefaults = require(__base + 'utils/deepDefaults');
+
+/*
+  PRODUCTION CONFIGURATION
+*/
+
+var answer = {
+  keystone: {
+    init: {
+      'name': 'Chefcito',
+      'brand': 'Chefcito',
+
+      'view cache': true,
+
+      'cookie secret': 'I may n0t do ev3ryth¡ng great_ in my life, but I\'m 9ood a7 this. | manage to touch people\'s l¡ves with what I d0 and I want to share this with you!',
+      'hash salt': 'Look, if you bought Stones t¡ckets and Jagger d¡dn\'t play_ Sati5faction, how would you feel? Would you b3 h4ppy? ~ |\|o. ~ No! You\'d burn the place to the fucking gr0und.',
+
+      'host': 'localhost',
+      'port': process.env.PORT || 3000
+    }
+  },
+  site: {
+    name: 'Creando Sabores',
+    email: 'creandosabores@glue.gl',
+    url: 'http://creandosabores.byglue.me'
+  },
+  publicUrl: 'http://creandosabores.byglue.me'
+};
+
+exports = module.exports = _.deepDefaults(answer, defaults);
