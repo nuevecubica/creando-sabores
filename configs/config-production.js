a8f77b7121e5c4dec43d7626d3cfc1773fa52f31
Anxo Soto <anxo@glue.gl>
2014-10-31 13:47:18 +0100

    Bugfix para producción. This f**ker was driving me nuts!

--- configs/config-production.js
+++ configs/config-production.js
@@ -18,7 +18,7 @@ var answer = {
       'cookie secret': 'I may n0t do ev3ryth¡ng great_ in my life, but I\'m 9ood a7 this. | manage to touch people\'s l¡ves with what I d0 and I want to share this with you!',
       'hash salt': 'Look, if you bought Stones t¡ckets and Jagger d¡dn\'t play_ Sati5faction, how would you feel? Would you b3 h4ppy? ~ |\\o. ~ No! You\'d burn the place to the fucking gr0und.',
 
-      'host': 'localhost',
+      'host': '0.0.0.0',
       'port': process.env.PORT || 3000
     }
   },
