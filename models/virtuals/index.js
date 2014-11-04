14894414d8347c914bebb7e5974ccebe1aea75cd
Miguel Sánchez <miguel@glue.gl>
2014-11-04 17:17:48 +0100

    userVirtuals.js: Added virtual file for user model index.js: Added userVirtuals user.js: Updated virtuals

--- models/virtuals/index.js
+++ models/virtuals/index.js
@@ -3,6 +3,7 @@
  * @type {Object}
  */
 exports = module.exports = {
+  user: require('./userVirtuals.js'),
   recipe: require('./recipeVirtuals.js'),
   menu: require('./menuVirtuals.js'),
   contest: require('./contestVirtuals.js'),
