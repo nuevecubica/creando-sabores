1cb49b39deb41fd817d6138b1c4f33ab17646fa7
Miguel Sánchez <miguel@glue.gl>
2014-11-04 08:39:58 +0100

    menuVirtuals.js: Added virtuals for menu index.js: Added menu virtuals

--- models/virtuals/index.js
+++ models/virtuals/index.js
@@ -4,6 +4,7 @@
  */
 exports = module.exports = {
   recipe: require('./recipeVirtuals.js'),
+  menu: require('./menuVirtuals.js'),
   contest: require('./contestVirtuals.js'),
   question: require('./questionVirtuals.js'),
   tip: require('./tipVirtuals.js')
