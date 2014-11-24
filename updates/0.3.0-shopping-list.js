7a0a62b366c69dab3c357c3b522659c519e482ad
Anxo Soto <anxo@glue.gl>
2014-11-24 12:52:44 +0100

    ShoppingList: Update del anterior modelo al nuevo.

---
+++ updates/0.3.0-shopping-list.js
@@ -0,0 +1,58 @@
+var keystone = require('keystone'),
+  async = require('async'),
+  User = keystone.list('User');
+
+function updateModel(done) {
+
+  // Read them without Mongoose to get the old schema values
+  User.model.collection.find({
+    schemaVersion: 2
+  }, function(err, cursor) {
+    if (err) {
+      return done(err);
+    }
+
+    function processItem(err, item) {
+      if (err) {
+        return done(err);
+      }
+      if (item === null) {
+        return done();
+      }
+      console.log('Updating:', item);
+      var shopping = [];
+      if (item.shopping) {
+        shopping = item.shopping.map(function(a) {
+          return {
+            recipe: a,
+            myIngredients: []
+          };
+        });
+      }
+
+      User.model.update({
+          _id: item._id
+        }, {
+          $set: {
+            'schemaVersion': 3,
+            'shopping': shopping
+          }
+        }, {
+          multi: true
+        },
+        function(err) {
+          if (err) {
+            return done(err);
+          }
+          cursor.nextObject(processItem);
+        });
+
+    }
+
+    cursor.nextObject(processItem);
+
+  });
+
+}
+
+exports = module.exports = updateModel;
