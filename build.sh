8e9f783cb2e4e75c2871aba2bef6b2a249d6ca3a
Anxo Soto <anxo@glue.gl>
2017-03-01 12:06:34 +0100

    Locked some versions, fixed some deprecated methods, and fixed build step of deploy system.

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bower install
+./node_modules/.bin/grunt build
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
+$(window).on('load', function() {
   var chef = window.chef;
 
   $('.ui.checkbox').checkbox();
